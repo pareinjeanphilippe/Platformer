ebd57788795e80159936e3e5e8b1cc2b493c19cb
Parein Jean Philippe <pareinjeanphilippe@outlook.fr>
2025-11-04 17:51:21 +0100

    Saut du personnage
    Version du projet avec la gestion du saut du personnage.

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -4,11 +4,15 @@ using UnityEngine.InputSystem;
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private LayerMask lm;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator animator;
     private SpriteRenderer sp;
+    private bool isGrounded;
+    private Transform groundCheck; 
 
     void Awake()
     {
@@ -16,9 +20,15 @@ public class CharacterController2D : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sp = GetComponent<SpriteRenderer>();
+        groundCheck = transform.Find("GroundCheck");       
     }
 
-    void OnMove(InputValue value) => moveInput = value.Get<Vector2>();
+    void OnMove(InputValue value) => moveInput = value.Get<Vector2>();   
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed && isGrounded) rb.linearVelocityY = jumpForce;
+    }
     
     void Update()
     {
@@ -26,6 +36,13 @@ public class CharacterController2D : MonoBehaviour
         animator.SetFloat("val_x", Mathf.Abs(moveInput.x));
         if (moveInput.x < 0) sp.flipX = true;
         if (moveInput.x > 0) sp.flipX = false;
+
+        //Perosnnage au sol ?
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, lm);
+
+        //animation de saut et de fall down
+        animator.SetFloat("vel_y", rb.linearVelocityY);
+        animator.SetBool("isGrounded", isGrounded);    
     }
 
     void FixedUpdate()
